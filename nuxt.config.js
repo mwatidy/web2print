8786ade6c5dddff98939e3b7d38c1e0614c925c4
Mahmoud ElWatidy <mahmoud.watidy@gmail.com>
2021-08-17 15:33:37 +0200

    refactor: config update

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Web 2 Print',
+    title: 'Web 2 Print Designer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
